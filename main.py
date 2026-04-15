ccdded848a5ef9217f1e8fe4354a96b70c6f7281
TimeAlex <89321045+TimeAlex1@users.noreply.github.com>
2026-04-15 08:49:18 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+﻿import lmstudio as lms
+import pyperclip
+import pyautogui
+
+pasted_text = pyperclip.paste()
+
+model = lms.llm("google/gemma-3-4b")
+result = str(model.respond(pasted_text + " (Do not include polite filler phrases like 'Here is the answer' or 'I hope this helps', additionally do not add a reply to this parameter like 'Sure, here is...')")).replace("*", "")
+pyautogui.press('_')
+
+pyperclip.copy(result)
+
+
